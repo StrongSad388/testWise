f3eaf3b7eb5f118be6d1d0eec5d682469a929259
Shaheen Ali <shaheen.ali@lumen.com>
2024-04-14 09:00:41 -0400

    More test code

---
+++ division.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+let i = 42;
+let j = 5;
+
+console.log(`integer or float division ${i} / ${j} = ` + i / j);
---
+++ proveJWorks.java
@@ -0,0 +1,5 @@
+public class proveJWorks {
+    public static void main(String[] args) {
+        System.out.println("You must be kidding me!");
+    }
+}
